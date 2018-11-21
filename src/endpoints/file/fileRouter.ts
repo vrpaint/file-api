f4ec1d66aa87c0d1227129807491ccd2cb229130
Pavol Hejny <me@pavolhejny.com>
2018-11-21 02:42:37 +0100

    working download

--- src/endpoints/file/fileRouter.ts
+++ src/endpoints/file/fileRouter.ts
@@ -1,5 +1,6 @@
 import { Router, NextFunction, Request, Response } from 'express';
 import { config, S3 } from 'aws-sdk';
+import * as multer from 'multer';
 
 export const fileRouter = Router();
 const bucketName = process.env.AWS_S3_BUCKET_NAME || '';
@@ -9,25 +10,46 @@ config.update({
     region: process.env.AWS_REGION,
 });
 const s3 = new S3();
-//const expiration = 60;
 
-fileRouter.get('/file/:id', uploadFile);
 
-async function uploadFile(req: Request, res: Response, next: NextFunction) {
+//const storage = multer.memoryStorage()
+//const upload = multer({storage});
+
+
+fileRouter.get('/file/:id', fileGet);
+//fileRouter.post('/file/:id', upload.single("file"), awsWorker.doUpload);
+
+async function fileGet(req: Request, res: Response, next: NextFunction) {
     try {
         const parameters = {
             Bucket: bucketName,
-            Key: req.params.id,
-            // Expires: expiration,
+            Key: req.params.id
         };
-        // const url = s3.getSignedUrl('getObject', parameters);
-        // res.send(url);
+
         const { Body, ContentType } = await s3.getObject(parameters).promise();
         res.set('Content-Type', ContentType);
-        // TODO: still forces the user to download the image...
         res.set('Content-Disposition', 'inline');
         res.send(Body);
+
+
     } catch (error) {
         next(error);
     }
 }
+
+
+
+ 
+/*
+async function filePost(req: Request, res: Response, next: NextFunction) {
+	
+
+	s3.upload(params, (err, data) => {
+		if (err) {
+			res.status(500).json({error:"Error -> " + err});
+		}
+		res.json({message: 'File uploaded successfully! -> keyname = ' + req.file.originalname});
+	});
+}
+*/
+ 
